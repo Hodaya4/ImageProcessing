b6c27f9426e301a5842a8732c26d4ae5e67f8f1f
Hodaya Gamliel <Hodaya4799@gmail.com>
2022-06-26 11:06:23 +0300

    need to save imageicon in file

--- src/main/java/Filters.java
+++ src/main/java/Filters.java
@@ -1,65 +1,94 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Filters {
     private BufferedImage image;
+    private File out;
 
     public Filters(BufferedImage image) {
         this.image = image;
+        this.out = new File("C:\\filterPics\\Basic.png");
+        try {
+            ImageIO.write(image , "png" , out);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public BufferedImage grayScale() {
-        BufferedImage tempImage = this.image;
-        for (int x = 0; x < tempImage.getWidth(); x++) {
-            for (int y = 0; y < tempImage.getHeight(); y++) {
-                int pixel1 = tempImage.getRGB(x, y);
-                Color color1 = new Color(pixel1);
-                int red = color1.getRed();
-                int green = color1.getGreen();
-                int blue = color1.getBlue();
-                int average = (red + green + blue) / 3;
-                Color color = new Color(average, average, average);
-                tempImage.setRGB(x, y, color.getRGB());
+        BufferedImage tempImg = null;
+        try {
+            tempImg = ImageIO.read(this.out);
+            for (int x = 0; x < tempImg.getWidth(); x++) {
+                for (int y = 0; y < tempImg.getHeight(); y++) {
+                    int pixel1 = tempImg.getRGB(x, y);
+                    Color color1 = new Color(pixel1);
+                    int red = color1.getRed();
+                    int green = color1.getGreen();
+                    int blue = color1.getBlue();
+                    int average = (red + green + blue) / 3;
+                    Color color = new Color(average, average, average);
+                    tempImg.setRGB(x, y, color.getRGB());
+                }
             }
-        } return tempImage;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tempImg;
     }
 
     public BufferedImage colorShiftRight() {
-        BufferedImage tempImage = this.image;
-        for (int x = 0; x < tempImage.getWidth(); x++) {
-            for (int y = 0; y < tempImage.getHeight(); y++) {
-                int pixel1 = tempImage.getRGB(x, y);
-                Color color1 = new Color(pixel1);
-                int red = color1.getRed();
-                int green = color1.getGreen();
-                int blue = color1.getBlue();
-                int tempRed = red;
-                red = green;
-                green = blue;
-                blue = tempRed;
-                Color color = new Color(red, green, blue);
-                tempImage.setRGB(x, y, color.getRGB());
+        BufferedImage tempImg = null;
+        try {
+            tempImg = ImageIO.read(this.out);
+            for (int x = 0; x < tempImg.getWidth(); x++) {
+                for (int y = 0; y < tempImg.getHeight(); y++) {
+                    int pixel1 = tempImg.getRGB(x, y);
+                    Color color1 = new Color(pixel1);
+                    int red = color1.getRed();
+                    int green = color1.getGreen();
+                    int blue = color1.getBlue();
+                    int tempRed = red;
+                    red = green;
+                    green = blue;
+                    blue = tempRed;
+                    Color color = new Color(red, green, blue);
+                    tempImg.setRGB(x, y, color.getRGB());
+                }
             }
-        } return tempImage;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tempImg;
     }
 
     public BufferedImage colorShiftLeft() {
-        for (int x = 0; x < image.getWidth(); x++) {
-            for (int y = 0; y < image.getHeight(); y++) {
-                int pixel1 = image.getRGB(x, y);
-                Color color1 = new Color(pixel1);
-                int red = color1.getRed();
-                int green = color1.getGreen();
-                int blue = color1.getBlue();
-                int tempRed = red;
-                red = blue;
-                blue = green;
-                green = tempRed;
-                Color color = new Color(red, green, blue);
-                image.setRGB(x, y, color.getRGB());
+        BufferedImage tempImg = null;
+        try {
+            tempImg = ImageIO.read(this.out);
+            for (int x = 0; x < tempImg.getWidth(); x++) {
+                for (int y = 0; y < tempImg.getHeight(); y++) {
+                    int pixel1 = tempImg.getRGB(x, y);
+                    Color color1 = new Color(pixel1);
+                    int red = color1.getRed();
+                    int green = color1.getGreen();
+                    int blue = color1.getBlue();
+                    int tempRed = red;
+                    red = blue;
+                    blue = green;
+                    green = tempRed;
+                    Color color = new Color(red, green, blue);
+                    tempImg.setRGB(x, y, color.getRGB());
+                }
             }
-        } return image;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tempImg;
     }
 
 
